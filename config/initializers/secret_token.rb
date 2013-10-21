91548abf769c1c5bbe5736a5283a0ac85c6fa0ff
Ali Ismayilov <ali@ismailov.info>
2013-10-21 23:10:37 +0500

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rottenpotatoes::Application.config.secret_key_base = '0602350a3c5faa58aec66a5a1e04746e5e97c77dfe801d9dec8681cba18093f585907a766ef5c56c8d9db7f3104701f35388557ed0a61012529195c542453e25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rottenpotatoes::Application.config.session_store :cookie_store, key: '_rottenpotatoes_session'
