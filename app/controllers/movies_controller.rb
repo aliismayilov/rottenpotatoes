bd2fb64d9b697f94bcf4904932053c0f651c1a81
Ali Ismayilov <ali@ismailov.info>
2013-10-22 00:54:41 +0500

    add sorting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.order params[:sort]
   end
 
   # GET /movies/1
