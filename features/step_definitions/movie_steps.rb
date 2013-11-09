729c8496ed5d112aae13e5e811a20875e515a635
Ali Ismayilov <ali@ismailov.info>
2013-11-08 23:27:22 -0200

    add pending movie steps

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,8 @@
+Given(/^the following movies exist:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the director of "(.*?)" should be "(.*?)"$/) do |movie_name, director|
+  pending # express the regexp above with the code you wish you had
+end
