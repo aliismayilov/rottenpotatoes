08fe62fa22a4151846c2774201e6b20c4480549d
Ali Ismayilov <ali@ismailov.info>
2013-11-08 23:35:59 -0200

    movie table step

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,6 +1,9 @@
-Given(/^the following movies exist:$/) do |table|
+Given /^the following movies exist:$/ do |table|
   # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  # pending # express the regexp above with the code you wish you had
+  table.rows.each do |row|
+    Movie.create! title: row[0], rating: row[1], director: row[2], release_date: row[3]
+  end
 end
 
 Then(/^the director of "(.*?)" should be "(.*?)"$/) do |movie_name, director|
